ec8962a8dc4eb6e0efab6fc691e37bc388bbb11d
Di Wu <CaptainAwesome@Dis-Mac-Air.hitronhub.home>
2018-02-15 23:39:50 -0800

    added Logging Wrapper.js

--- arrayMappingChallenge.js
+++ arrayMappingChallenge.js
@@ -4,11 +4,15 @@ var input = [
   { x: 8, y: 15 }
 ];
 var result = input.map(function(obj) {
-  var z = 0;
-  z = (Math.pow(obj.x, 2)) + (Math.pow(obj.y, 2));
+  var z = (Math.pow(obj.x, 2)) + (Math.pow(obj.y, 2));
   return Math.sqrt(z);
 });
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
 console.log(result[2] === 17);
+
+//function(o) { return Math.sqrt(o.x * o.x + o.y * o.y); }
+// var result2 = input.map(o => Math.sqrt(o.x * o.x + o.y * o.y))
+// var clamped = inputs.map(x => Math.max(0, Math.min(1, x)))
+// var crap = some_numbers.filter(x => x >= 0).map(x => x * x).reduce((acc, ele) => acc + ele, 0);
